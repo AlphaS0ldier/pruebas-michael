488f5ec036a8ac7efa29a5b81792e1462f616cc2
Michael <villaluzmichael19@gmail.com>
2023-04-12 09:49:47 +0200

    Código fuente inicial

---
+++ src/archivo_java_git/Archivo_Java_Git.java
@@ -0,0 +1,12 @@
+package archivo_java_git;
+
+
+public class Archivo_Java_Git {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hola Mundo!");
+        
+    }
+
+}
